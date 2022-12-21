50d07639849527c0b1a00cbc765a06648703ef65
Fastattack <120670645+fastattackv@users.noreply.github.com>
2022-12-21 12:05:17 +0100

    Update, adding save system to save on wich channel you play

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import pickle
 
 load_dotenv(dotenv_path="config")
 
 default_intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=default_intents)
+
+serv_non_def = "pas de serv"
+with open("save serveurs bot", "rb") as f:
+    dico_serv = pickle.load(f)
 
 # variables de devine
 joueur1 = False
@@ -17,7 +22,7 @@ id_joueur2 = False
 var_devine = False
 var_devine_j1 = False
 var_devine_j2 = False
-nombre_à_d = 1
+nombre_a_d = 1
 points = 0
 liste_points = []
 
@@ -25,6 +30,13 @@ liste_points = []
 @bot.event
 async def on_ready():
     print("Le bot est connecté.")
+
+
+@bot.event
+async def on_guild_join(serveur):
+    global serv_non_def
+    serv_non_def = serveur
+    return serv_non_def
 
 
 @bot.event
@@ -45,52 +57,53 @@ async def on_message(message):
     global var_devine
     global var_devine_j1
     global var_devine_j2
-    global nombre_à_d
+    global nombre_a_d
     global points
     global liste_points
+    channel_jeu = dico_serv[str(message.guild)]
     if message.author.name == "FastBot":
         pass
     else:
         print(message.author)
         print(message.channel)
         print(message.content)
-    if message.content.lower() == "ping":
+    if message.content.lower() == "ping" and str(message.channel) == channel_jeu:
         await message.channel.send("pong")
     # début devine
-    elif str(message.channel) == "jeu-fastbot" and message.content.lower() == "je veux jouer" and var_devine == False:
+    elif str(message.channel) == channel_jeu and message.content.lower() == "je veux jouer" and var_devine == False:
         joueur1 = message.author
         id_joueur1 = message.author.id
         var_devine = True
         await message.channel.send("à quoi ?")
-    elif var_devine and str(message.channel) == "jeu-fastbot" and message.author == joueur1 and message.content.lower() == "à devine":
+    elif var_devine and str(message.channel) == channel_jeu and message.author == joueur1 and message.content.lower() == "à devine":
         await message.channel.send(f"qui joue avec {joueur1} ?")
-    elif var_devine and str(message.channel) == "jeu-fastbot" and message.content.lower() == "moi":
+    elif var_devine and str(message.channel) == channel_jeu and message.content.lower() == "moi":
         joueur2 = message.author
         id_joueur2 = message.author.id
         await message.channel.send(f"<@{id_joueur1}> devine (le nombre entre 1 et 99)")
         var_devine_j1 = True
-        nombre_à_d = random.randint(1, 99)
-    elif var_devine and str(message.channel) == "jeu-fastbot":
+        nombre_a_d = random.randint(1, 99)
+    elif var_devine and str(message.channel) == channel_jeu:
         if var_devine_j1 and message.author == joueur1:
             nombre_d = int(message.content)
-            if nombre_d == nombre_à_d:
+            if nombre_d == nombre_a_d:
                 await message.channel.send("bonne réponse")
                 points += 1
                 liste_points.append(points)
                 await message.channel.send(f"<@{id_joueur2}> devine (le nombre entre 1 et 99)")
                 var_devine_j1 = False
                 var_devine_j2 = True
-                nombre_à_d = random.randint(1, 99)
+                nombre_a_d = random.randint(1, 99)
                 points = 0
-            elif nombre_d < nombre_à_d:
+            elif nombre_d < nombre_a_d:
                 await message.channel.send("plus")
                 points += 1
-            elif nombre_d > nombre_à_d:
+            elif nombre_d > nombre_a_d:
                 await message.channel.send("moins")
                 points += 1
         elif var_devine_j2 and message.author == joueur2:
             nombre_d = int(message.content)
-            if nombre_d == nombre_à_d:
+            if nombre_d == nombre_a_d:
                 await message.channel.send("bonne réponse")
                 points += 1
                 liste_points.append(points)
@@ -110,19 +123,18 @@ async def on_message(message):
                 joueur2 = False
                 points = 0
                 liste_points = []
-            elif nombre_d < nombre_à_d:
+            elif nombre_d < nombre_a_d:
                 await message.channel.send("plus")
                 points += 1
-            elif nombre_d > nombre_à_d:
+            elif nombre_d > nombre_a_d:
                 await message.channel.send("moins")
                 points += 1
-
-
-@bot.command(name="del")
-async def delete(ctx, nombre_of_messages: int):
-    messages_à_supprimer = await ctx.channel.history(limit=nombre_of_messages + 1).flatten()
-    for chaque_message in messages_à_supprimer:
-        await chaque_message.delete()
-
+    elif message.content == "<@1037734637285421197> .channel.jeu" and message.author.guild_permissions.administrator:
+        dico_serv[str(message.guild)] = str(message.channel)
+        with open("save serveurs bot", "wb") as f:
+            pickle.dump(dico_serv, f)
+        await message.channel.send(f"le salon de jeu est <#{message.channel.id}>")
+    elif message.content  == "<@1037734637285421197> .channel.jeu" and message.author.guild_permissions.administrator == False:
+        await message.channel.send("tu n'es pas admin")
 
 bot.run(os.getenv("TOKEN"))
